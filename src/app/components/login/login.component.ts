fb9df2ada1c5259808e6b6f78c0f34549037209e
manulaPankaja <pankajabad1998@gmail.com>
2023-03-21 19:18:39 +0530

    added some styles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  public form ={
+    email:null,
+    password:null
+  };
+
+  constructor(private http:HttpClient){
+
+  }
+
+  onSubmit(){
+    return this.http.post('http://127.0.0.1:8000/api/auth/login', this.form).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+      );
+  }
 
 }
